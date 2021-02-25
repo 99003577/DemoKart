c334933d165754401ba5b8d00e786c03d03e1c0a
BhanuSri_Madeti <78855795+99003665@users.noreply.github.com>
2021-02-25 23:40:33 +0530

    Update Payment.cs

--- DemoKartBL/Payment.cs
+++ DemoKartBL/Payment.cs
@@ -5,7 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DemoKartBL
+{using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKartBL
 {
+    public class Payment
+    {
+        enum Mode { ByCash=1,ByCard}
+        public double CalculateCGST(int num, double price)
+        {
+            if (num == (int)Mode.ByCash)
+            {
+                double Price = 0;
+                Price = price;
+                return Price;
+            }
+            else
+            {
+                double Price = 0;
+                Price = price + (price * 0.035) ;
+                return Price;
+            }   
+        }
+        public double CalculateSGST(int num, double price)
+        {
+            if (num == (int)Mode.ByCash)
+            {
+                double Price = 0;
+                Price = price ;
+                return Price;
+            }
+            else
+            {
+                double Price = 0;
+                Price = price + (price * 0.035 );
+                return Price;
+            }
+        }
+    }
+}
+
    public class Payment
     {
         public double Payment_method(double finalprice)
